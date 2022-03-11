f84cc327bafe0eabb7a56a514e8f396bfea31f5a
asheersaeed720 <asheersaeed720@gmail.com>
2022-03-11 21:54:05 +0500

    create ticket screen UI semi done

--- lib/src/ticket/create_ticket_screen.dart
+++ lib/src/ticket/create_ticket_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:game_ticket/utils/constants.dart';
 
 class CreateTicketScreen extends StatelessWidget {
   static const String routeName = '/create-ticket';
@@ -8,14 +9,136 @@ class CreateTicketScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/background.png"),
-            fit: BoxFit.cover,
+      floatingActionButton: Container(
+        decoration: BoxDecoration(
+            border: Border.all(width: 2.0, color: primaryColor),
+            borderRadius: BorderRadius.circular(32.0)),
+        child: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.white,
+          child: const Icon(
+            Icons.border_all_rounded,
+            color: primaryColor,
           ),
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/background.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 62.0, right: 16.0, left: 16.0),
+            child: Column(
+              children: [
+                _buildAppBarView(),
+                const SizedBox(height: 20.0),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.7,
+                  padding: const EdgeInsets.only(top: 30.0, bottom: 28.0, right: 16.0, left: 16.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(24.0),
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Colors.white.withOpacity(0.3),
+                        Colors.white.withOpacity(0.1),
+                      ],
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text('Your Game Name', style: kTitleStyle),
+                          const SizedBox(width: 10.0),
+                          const Icon(Icons.edit, color: primaryColor, size: 18.0),
+                        ],
+                      ),
+                      const SizedBox(height: 10.0),
+                      Text('Select a Game', style: kBodyStyle),
+                      const SizedBox(height: 10.0),
+                      Container(
+                        padding: const EdgeInsets.all(14.0),
+                        decoration: BoxDecoration(
+                          color: primaryColor,
+                          borderRadius: BorderRadius.circular(32.0),
+                        ),
+                        child: const Icon(Icons.arrow_forward_ios_rounded, color: Colors.white),
+                      ),
+                      const SizedBox(height: 10.0),
+                      Text('Add Player', style: kBodyStyle),
+                      const SizedBox(height: 10.0),
+                      Container(
+                        padding: const EdgeInsets.all(14.0),
+                        decoration: BoxDecoration(
+                          color: primaryColor,
+                          borderRadius: BorderRadius.circular(32.0),
+                        ),
+                        child: const Icon(Icons.arrow_forward_ios_rounded, color: Colors.white),
+                      ),
+                      const SizedBox(height: 10.0),
+                      Text('Details', style: kBodyStyle),
+                      const SizedBox(height: 10.0),
+                      Container(
+                        padding: const EdgeInsets.all(14.0),
+                        decoration: BoxDecoration(
+                          color: primaryColor,
+                          borderRadius: BorderRadius.circular(32.0),
+                        ),
+                        child: const Icon(Icons.arrow_forward_ios_rounded, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAppBarView() {
+    return Row(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(12.0),
+          decoration: BoxDecoration(
+            border: Border.all(width: 2.0, color: primaryColor),
+            borderRadius: BorderRadius.circular(32.0),
+          ),
+          child: const Icon(
+            Icons.arrow_back_ios_rounded,
+            color: primaryColor,
+          ),
+        ),
+        const SizedBox(width: 20.0),
+        RichText(
+          text: const TextSpan(
+            text: 'Create ',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Poppins',
+            ),
+            children: <TextSpan>[
+              TextSpan(text: 'Game', style: TextStyle(color: primaryColor)),
+              TextSpan(text: ' Ticket!'),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -8,9 +8,14 @@ const Color textFieldColor = Color.fromARGB(255, 238, 236, 237);
 
 //? TextStyle
 TextStyle kTitleStyle = const TextStyle(
-  color: primaryColor,
+  color: Colors.white,
   fontWeight: FontWeight.bold,
   fontSize: 18.0,
+);
+TextStyle kBodyStyle = const TextStyle(
+  color: Colors.white,
+  fontSize: 15.0,
+  fontWeight: FontWeight.bold,
 );
 
 extension StringExtension on String {
